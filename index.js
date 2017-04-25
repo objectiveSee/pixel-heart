6d7094bf1f98978d3f94953289139666b29c99fc
objectiveSee <dan.ricciotti@gmail.com>
2017-04-25 15:08:46 -0400

    comments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// var heart = require('./heart.js');
-
 var fs 					= require('fs')
 var _ 					= require('underscore')
 var makerjs 			= require('makerjs')
@@ -12,13 +10,16 @@ var StrokeModel 		= require('./stroke-model.js')
 /** 
  * Properties of design
  */
-var inner_heart_width = 305
-var wood_thickness = 3.22 // in mm
-var inner_stroke_around_heart = wood_thickness * 2
-var outer_stroke_around_heart = 25.4
-var grid_spacing_between_models = 15
+var inner_heart_width = 305					// in mm
+var wood_thickness = 3.22 					// in mm
+var inner_stroke_around_heart = 6.44		// in mm
+var outer_stroke_around_heart = 25.4		// in mm
+var grid_spacing_between_models = 15		// for laying out the final boxes and models
+var box_depth = 18 							// the height of the final product in mm
 
-// derived values
+/**
+ * derived values
+ */
 var wrap_boxes_after_width = inner_heart_width + inner_stroke_around_heart*2
 var box_layout_spacing = wood_thickness/2
 var notch_width = wood_thickness
@@ -27,26 +28,25 @@ var notch_width = wood_thickness
 // Create the heart model & scale to desired size
 var heart = Heart(inner_heart_width)
 
-// Create expanded model & notch it with boxes only
+// notching options
 var heart_notch_options = {
 	thickness: wood_thickness,
 	pattern: [-1],
 	notch_width: notch_width
 }
 
-// Create 2nd expanded model & notch it
-heart_notch_options.notches_are_boxes = false
+// Create outer expanded model & notch it
+heart_notch_options.return_notches_without_model = false
 var outer_model = StrokeModel(heart, outer_stroke_around_heart)
 var notched_heart_outer = notcher.notchModel(outer_model, heart_notch_options)
 
-// Create 1st expanded model & notch it
-heart_notch_options.notches_are_boxes = true
+// Create inner expanded model & notch it
+heart_notch_options.return_notches_without_model = true
 var inner_model = StrokeModel(heart, inner_stroke_around_heart)
 var notched_heart_inner = notcher.notchModel(inner_model, heart_notch_options)
 
 
-// Create boxes & notch
-var boxDepth = 18
+// Configure boxes that are the vertical walls of the final product
 var box_notch_options = {
 	thickness: wood_thickness,
 	pattern: [1,1,-1,-1],	// bottom is always male, top always female, sides are one of each
@@ -54,29 +54,32 @@ var box_notch_options = {
 	is_walls: true
 }
 
-// Outer boxes
-var boxes_outer = Boxes.makeBoxWallsAlongModelPerimeter(outer_model, boxDepth, wood_thickness)
-
+// Create outer boxes
+var boxes_outer = Boxes.makeBoxWallsAlongModelPerimeter(outer_model, box_depth, wood_thickness)
 var notched_boxes_outer = notcher.notchModelsInParent(boxes_outer, box_notch_options)
+
+// Align boxes in a grid
 LayoutGrid(notched_boxes_outer, {
 	x_spacing: box_layout_spacing,
 	y_spacing: box_layout_spacing,
 	max_width: wrap_boxes_after_width
-})	// re-align in a grid
+})
+
 // move boxes above the heart in final SVG
-// ASSUME: y position of the heart is 0
 var heart_extents = makerjs.measure.modelExtents(outer_model)
 makerjs.model.move(notched_boxes_outer, [0,heart_extents.high[1]+grid_spacing_between_models])
 
 
-// Inner boxes
-var boxes_inner = Boxes.makeBoxWallsAlongModelPerimeter(inner_model, boxDepth, wood_thickness)
+// Create inner boxes
+var boxes_inner = Boxes.makeBoxWallsAlongModelPerimeter(inner_model, box_depth, wood_thickness)
 var notched_boxes_inner = notcher.notchModelsInParent(boxes_inner, box_notch_options)
+
+// Align boxes in a grid
 LayoutGrid(notched_boxes_inner, {
 	x_spacing: box_layout_spacing,
 	y_spacing: box_layout_spacing,
 	max_width: wrap_boxes_after_width
-})	// re-align in a grid
+})
 
 // move boxes above the other notched boxes in final SVG
 var outer_boxes_extents = makerjs.measure.modelExtents(notched_boxes_outer)
@@ -94,7 +97,7 @@ var export_options = {
 // Name layers
 notched_heart_inner.layer = 'notched_heart_inner'
 notched_heart_outer.layer = 'notched_heart_outer'
-inner_model.layer = 'inner_model'
+//inner_model.layer = 'inner_model'
 notched_boxes_outer.layer = 'notched_boxes_outer'
 notched_boxes_inner.layer = 'notched_boxes_inner'
 
@@ -103,7 +106,7 @@ var final_model = {
 		//heart: heart,
 		notched_heart_inner: notched_heart_inner,
 		notched_heart_outer: notched_heart_outer,
-		inner_model: inner_model,
+//		inner_model: inner_model,
 		notched_boxes_outer: notched_boxes_outer,
 		notched_boxes_inner: notched_boxes_inner
 	}
@@ -111,29 +114,29 @@ var final_model = {
 
 var svg = makerjs.exporter.toSVG(final_model,export_options);
 
-var file = "<!DOCTYPE html>\n"+
-"<html>\n"+
-"<body>\n\n"+
-svg+
-"\n\n</body>\n"+
-"</html>\n"
-
 var output_as_svg = true
 var filename, output
 
+// Option to export as SVG or HTML file
 if ( output_as_svg ) {
 	filename = 'out.svg'
 	output = svg
 } else {
+	var file = "<!DOCTYPE html>\n"+
+		"<html>\n"+
+		"<body>\n\n"+
+		svg+
+		"\n\n</body>\n"+
+		"</html>\n"
 	filename = 'out.html'
 	output = file
 }
+
 
 fs.writeFile(filename, output, function(err) {
     if(err) {
         return console.log(err);
     }
+    console.log('The file '+filename+' was saved!')
+})
 
-    console.log('The file '+filename+' was saved!');
-
-});
